e3d19634930a6408af26d987df41ebe210aaba21
Sanketh <Sanketh52@gmail.com>
2025-05-04 21:17:11 +0530

    Implemented exception handling

---
+++ src/main/java/com/scaler/productservicedemo/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.scaler.productservicedemo.exceptions;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
